5ae6e6874736013501d57f9bd2cca789490ed962
Arman <ak.pcnet@gmail.com>
2022-02-17 08:05:56 +0330

    test search3

--- pages/search.js
+++ pages/search.js
@@ -249,6 +249,7 @@ export async function getServerSideProps({ query }) {
   const priceFilter =
     price && price != "all"
       ? {
+          // price : 10-50
           //compare price with this two filter: gte(greater than) and lte(les than)
           price: {
             $gte: Number(price.split("-")[0]),
